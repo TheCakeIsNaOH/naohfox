db004d1deac3c8fc693d7174c876685fa682392b
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2021-08-14 15:14:05 -0500

    fix bootstrap

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ cd c:/
 mkdir mozilla-source
 cd mozilla-source
 wget https://hg.mozilla.org/mozilla-central/raw-file/default/python/mozboot/bin/bootstrap.py
-python3 bootstrap.py
+python3 bootstrap.py --no-interactive
 cd mozilla-central
 ./mach build
 ./mach package
