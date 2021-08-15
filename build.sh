ef9365117bd71e6d9e0b79a11a6839a09361917f
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2021-08-14 21:08:02 -0500

    fix dir name

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ mkdir mozilla-source
 cd mozilla-source
 wget https://hg.mozilla.org/mozilla-central/raw-file/default/python/mozboot/bin/bootstrap.py
 python3 bootstrap.py --no-interactive
-cd mozilla-central
+cd mozilla-unified
 ./mach build
 ./mach package
